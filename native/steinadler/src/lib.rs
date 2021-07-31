17af652223640029a290c34f047f66015360bc57
Weslei Juan Moser Pereira <wesleimsr@gmail.com>
2021-07-31 00:51:56 -0300

    Create TCP server

--- native/steinadler/src/lib.rs
+++ native/steinadler/src/lib.rs
@@ -2,6 +2,11 @@
 extern crate rustler;
 
 use rustler::Atom;
+use std::io::{Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
+use std::str::from_utf8;
+use std::string::String;
+use std::thread;
 
 mod atoms {
     atoms! {
@@ -10,13 +15,41 @@ mod atoms {
     }
 }
 
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n\r";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
 #[rustler::nif(schedule = "DirtyCpu")]
 fn bind(address: String, port: i64) -> Atom {
+    let addr = format!("{}:{}", address, port);
+    let listener = TcpListener::bind(&addr).unwrap();
+
+    println!("Server running at {}\r", addr);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("New connection: {}\r", stream.peer_addr().unwrap());
+        thread::spawn(move || handle_connection(stream));
+    }
+
     atoms::ok()
 }
 
 #[rustler::nif(schedule = "DirtyCpu")]
 fn connect(name: String, address: String, port: i64) -> Atom {
+    let addr = format!("{}:{}", address, port);
+    let mut stream = TcpStream::connect(&addr).unwrap();
+    println!("Sent Hello, awaiting reply...\r");
+    let msg = b"Hello!";
+    stream.write(msg).unwrap();
+    stream.read(&mut [0; 128]).unwrap();
     atoms::ok()
 }
 
--- test/steinadler_test.exs
+++ test/steinadler_test.exs
@@ -4,4 +4,8 @@ defmodule SteinadlerTest do
   test "bind localhost on port 4369" do
     assert :ok = Steinadler.bind("localhost", 4369)
   end
+
+  test "connect to node" do
+    assert :ok = Steinadler.connect("name", "localhost", 4369)
+  end
 end
