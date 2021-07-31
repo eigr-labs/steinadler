a1e0312e7b0df21de7839fd92283b5472c89b9ad
Weslei Juan Moser Pereira <wesleimsr@gmail.com>
2021-07-30 23:33:46 -0300

    Change return to atom

--- native/steinadler/src/lib.rs
+++ native/steinadler/src/lib.rs
@@ -1,18 +1,28 @@
+#[macro_use]
+extern crate rustler;
 
+use rustler::Atom;
 
-#[rustler::nif(schedule = "DirtyCpu")]
-fn bind(address: String, port: i64) -> bool {
-    true
+mod atoms {
+    atoms! {
+        ok,
+        error
+    }
 }
 
 #[rustler::nif(schedule = "DirtyCpu")]
-fn connect(name: String, address: String, port: i64) -> bool {
-    true
+fn bind(address: String, port: i64) -> Atom {
+    atoms::ok()
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn connect(name: String, address: String, port: i64) -> Atom {
+    atoms::ok()
 }
 
 #[rustler::nif]
-fn disconnect(name: String) -> bool {
-    true
+fn disconnect(name: String) -> Atom {
+    atoms::ok()
 }
 
 rustler::init!("Elixir.Steinadler.Native", [bind, connect, disconnect]);
--- test/steinadler_test.exs
+++ test/steinadler_test.exs
@@ -2,6 +2,6 @@ defmodule SteinadlerTest do
   use ExUnit.Case
 
   test "bind localhost on port 4369" do
-    assert true = Steinadler.bind("localhost", 4369)
+    assert :ok = Steinadler.bind("localhost", 4369)
   end
 end
