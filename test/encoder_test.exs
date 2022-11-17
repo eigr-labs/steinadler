363001a07e5ea246058cac75adaa510179e0ae40
Adriano Santos <sleipnir@bsd.com.br>
2022-11-17 14:46:54 -0300

    Add one more test

--- test/encoder_test.exs
+++ test/encoder_test.exs
@@ -4,12 +4,22 @@ defmodule EncoderTest do
   alias Steinadler.Encoder.ErlangEncoder
   alias SteinadlerTest.Support.Mod
 
-  test "encode/decode erlang term to binary" do
-    mod = %Mod{property: 1}
-    encoded = ErlangEncoder.encode(mod)
-    decoded = ErlangEncoder.decode(encoded)
+  describe "encode/decode" do
+    test "erlang term to binary and vice-versa" do
+      mod = %Mod{property: 1}
+      encoded = ErlangEncoder.encode(mod)
+      decoded = ErlangEncoder.decode(encoded)
 
-    assert is_binary(encoded.binary)
-    assert 1 == decoded.property
+      assert is_binary(encoded.binary)
+      assert 1 == decoded.property
+    end
+
+    test "decode and call function" do
+      func = fn a, b -> a + b end
+      encoded_func = ErlangEncoder.encode(func)
+      decoded_func = ErlangEncoder.decode(encoded_func)
+
+      assert 5 == decoded_func.(2, 3)
+    end
   end
 end
